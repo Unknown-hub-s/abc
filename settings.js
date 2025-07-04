bc0a86cde9a57be9c74260953161d557510e464a
Unknown-hub-s <Sandaruperera1230@gmail.com>
2025-07-04 19:41:20 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === 𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=0c5RDT7Y#PpOsOVCN3b5nZ9z-mzZh6UwUBalfrmH-z8XDhvc2lKg '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=32pQlDLS#EM2MbhJvScMPazpSfBf3MAlkrFxUzwuv-5rKwhdhFqI' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=9coHDJ5Q#OuyfLjTQKKwV7i2kLcqBupMTNeu5_0HYR7UhfWN59-s' : process.env.SESSION_ID,
 PORT: process.env.PORT === undefined ? "8000" : process.env.PORT,
 SESSION_NAME: process.env.PORT === undefined ? "asitha" : process.env.SESSION_NAME,
 POSTGRESQL_URL: process.env.POSTGRESQL_URL === undefined ? 'postgresql://postgres:@Asitha2005b@db.waiqbrnuxkjebghzhovz.supabase.co:5432/postgres' : process.env.POSTGRESQL_URL,
